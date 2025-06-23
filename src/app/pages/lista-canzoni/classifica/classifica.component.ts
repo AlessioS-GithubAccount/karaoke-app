973215912f20ae1bfceaa0c242659a9c5cba1a9b
AlessioSanna <noticebox.job@outlook.com>
2025-06-24 01:42:36 +0200

    correzione bug top20 component (duplicati e uppercase)

--- src/app/pages/lista-canzoni/classifica/classifica.component.ts
+++ src/app/pages/lista-canzoni/classifica/classifica.component.ts
@@ -18,11 +18,32 @@ export class ClassificaComponent implements OnInit {
   caricaTop20(): void {
     this.karaokeService.getTop20().subscribe({
       next: (data) => {
-        this.top20 = data;
+        // Rimuovo duplicati basati su "artista" + "canzone"
+        const uniqueMap = new Map<string, any>();
+        data.forEach(item => {
+          const key = `${item.artista.toLowerCase()}|${item.canzone.toLowerCase()}`;
+          if (!uniqueMap.has(key)) {
+            // Capitalizzo artista e canzone con prima lettera maiuscola e resto minuscolo
+            uniqueMap.set(key, {
+              ...item,
+              artista: this.capitalizeWords(item.artista),
+              canzone: this.capitalizeWords(item.canzone)
+            });
+          }
+        });
+        this.top20 = Array.from(uniqueMap.values());
       },
       error: (err) => {
         console.error('Errore nel caricamento della classifica:', err);
       }
     });
+  }
+
+  // Funzione helper per capitalizzare ogni parola (prima lettera maiuscola)
+  private capitalizeWords(str: string): string {
+    if (!str) return '';
+    return str.replace(/\w\S*/g, (txt) =>
+      txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    );
   }
 }
