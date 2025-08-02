4b4e8cf25bd6c461c336b2d8a46ee000a4680dfa
AlessioSanna <noticebox.job@outlook.com>
2025-08-02 21:25:09 +0200

    correttto import

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
