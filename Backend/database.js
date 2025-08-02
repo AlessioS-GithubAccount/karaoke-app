346834991877fa2ab53e60ae82a809797e3072b3
AlessioSanna <noticebox.job@outlook.com>
2025-08-02 17:08:10 +0200

    connesso db remoto host

--- Backend/database.js
+++ Backend/database.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mysql1990@',
-  database: 'karaokedb',
+  host: 'sql7.freesqldatabase.com',
+  user: 'sql7793261',
+  password: 'AiTxTszAmQ',
+  database: 'sql7793261',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
