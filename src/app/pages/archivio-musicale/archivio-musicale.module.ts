1068de6ea6a04dab8e496c3804e24b88098f492f
AlessioSanna <noticebox.job@outlook.com>
2025-08-07 17:19:04 +0200

    corretto file lazyloading archiviocanzoni

--- src/app/pages/archivio-musicale/archivio-musicale.module.ts
+++ src/app/pages/archivio-musicale/archivio-musicale.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ArchivioMusicaleComponent } from './archivio-musicale.component'; 
-import { RouterModule, Routes } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { ArchivioMusicaleComponent } from './archivio-musicale.component';
+
 
 const routes: Routes = [
   { path: '', component: ArchivioMusicaleComponent }
 ];
 
 @NgModule({
+  declarations: [ArchivioMusicaleComponent],
   imports: [  
     RouterModule.forChild(routes),
     CommonModule,
     TranslateModule,
+    ToastrModule,
+    MatDialogModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ArchivioMusicaleModule { }
