01d3eb66853fefcb81e0bebf3c0fa3690ec60cc0
AlessioSanna <noticebox.job@outlook.com>
2025-12-31 01:34:18 +0100

    bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: 'https://karaoke-app-6byu.onrender.com/api',
-  // endpoint WebSocket in prod (niente /api)
-  wsUrl: 'https://karaoke-app-6byu.onrender.com'
+
+  // ✅ base del socket (SENZA /api)
+  socketBaseUrl: 'https://karaoke-app-6byu.onrender.com',
 };
