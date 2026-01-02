072ca2e2c246982422c9c28bd738ae126f648bd3
AlessioSanna <noticebox.job@outlook.com>
2026-01-02 16:49:47 +0100

    elimino vecchio service deprecated

--- src/app/chat/chat-ui.service.ts
+++
@@ -1,4 +0,0 @@
-// DEPRECATED / COMPATIBILITY LAYER
-// Non creare un secondo servizio: re-exporta quello canonico.
-
-export { ChatRealtimeService, OnlineUser, ChatMessage } from './chat-realtime.service';
